42ad0df2de0619e9e64688d17e8fbc8aa6880c09
Tunde Adebayo <tundeadebayo@Tundes-Air.attlocal.net>
2020-08-14 11:08:00 -0500

    added files

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,43 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+//get a reference to the table body 
+var tbody = d3.select('tbody');
+
+//Level 1A - Write code that appends a table to your webpage and 
+//add new row of data for each UFO sighting
+
+data.forEach((UFOReport) => {
+    var row =tbody.append('tr');
+    Object.entries(UFOReport).forEach(([key, value]) => {
+        var cell = row.append('td')
+        cell.text(value);
+    });
+});
+ 
+
+//Level 1B - Use a date form in your HTML document and write JavaScript code that willl listen 
+//for events and search through the date/time column to find rows that match user input
+
+// //get a reference to the filter table button click 
+var   filterButton = d3.select('#filter-btn');
+//define on click event handler function    
+filterButton.on('click', function(){
+    //to clear table on new click 
+    tbody.html('');
+    //get a reference on the datetime input
+    var inputElement = d3.select('#datetime');
+    //extract the input date 
+    var inputDate = inputElement.property('value');
+    //define filter varibale and display rows matching search criteria 
+    var searchResults = tableData.filter(search => search.datetime === inputDate);
+    searchResults.forEach((UFOSearch) => {
+        var row =tbody.append('tr');
+        Object.entries(UFOSearch).forEach(([key, value]) => {
+            var cell = row.append('td')
+            cell.text(value);
+        });
+    });
+});
+
